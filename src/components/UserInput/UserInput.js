4ced5dbc534dfae29c00c5df0cc7ec8aa3eb883c
sharmas75 <71354509+sharmas75@users.noreply.github.com>
2023-11-10 14:50:09 -0700

    redo file hierarchy

---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import classes from './UserInput.module.css'
+
+
+const UserInput = (props) => {
+  const initalUserInput = {
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    duration: 10,
+  };
+
+  const [userInput, setUserInput] = useState(initalUserInput);
+
+  const submitHandler = (event) => {
+      event.preventDefault();
+      /* Code below is involved in passing data between components */
+      props.onCalculate(userInput);
+  };
+
+  const resetHandler = () => {
+    setUserInput(initalUserInput);
+  };
+
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: +value, //the "+" converts string value to a number
+      };
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes['input-group']}>
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("current-savings", event.target.value)
+            }
+            value={userInput["current-savings"]}
+            type="number"
+            id="current-savings"
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearly-contribution", event.target.value)
+            }
+            value={userInput["yearly-contribution"]}
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className={classes['input-group']}>
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            value={userInput["expected-return"]}
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("duration", event.target.value)
+            }
+            value={userInput["duration"]}
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className={classes.actions}>
+        <button type="reset" onClick={resetHandler} className={classes.buttonAlt}>
+          Reset
+        </button>
+        <button type="submit" className={classes.button}>
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
+
+export default UserInput;
